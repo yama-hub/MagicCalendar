38ad23bc1b3930812517e6ccb9094447e8ba33b3
yamamoto <yamamoto@airmac2.local>
2014-04-08 18:57:42 +0900

    Initial Commit

---
+++ MagicCalendar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MagicCalendar
+//
+//  Created by yamamoto on 2014/04/08.
+//  Copyright (c) 2014年 G.Yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MagicCalendar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MagicCalendar
+//
+//  Created by yamamoto on 2014/04/08.
+//  Copyright (c) 2014年 G.Yamamoto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
